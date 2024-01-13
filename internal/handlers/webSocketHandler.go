88041f0e7c25dc4bf5041bd3cb91da45393ceaa3
bipblop555 <tom.bonnet@hotmail.com>
2024-01-13 13:22:21 +0100

    websocket /ws/id

--- internal/handlers/webSocketHandler.go
+++ internal/handlers/webSocketHandler.go
@@ -2,12 +2,16 @@ package handlers
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"github.com/gorilla/websocket"
 	"github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -30,7 +34,7 @@ type WebSocketContext struct {
 	Listener *pq.Listener
 }
 
-func HandleWebSocketConnection(w http.ResponseWriter, r *http.Request, ctx *WebSocketContext) {
+func HandleWebSocketConnection(w http.ResponseWriter, r *http.Request, ctx *WebSocketContext, userId string) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println("Erreur lors de la mise à niveau du WebSocket :", err)
@@ -40,6 +44,8 @@ func HandleWebSocketConnection(w http.ResponseWriter, r *http.Request, ctx *WebS
 
 	fmt.Println("WebSocket connecté.")
 	clients[conn] = true
+
+	go WaitForNotification(ctx.Listener, userId, conn)
 
 	for {
 		_, _, err := conn.ReadMessage()
@@ -76,16 +82,22 @@ func StartSQL(c chan os.Signal) {
 		Listener: listener,
 	}
 
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		HandleWebSocketConnection(w, r, ctx)
+	http.HandleFunc("/ws/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+		HandleWebSocketConnection(w, r, ctx, id)
+	})
+	http.HandleFunc("/ws/", func(w http.ResponseWriter, r *http.Request) {
+		// Extraire l'ID de l'URL
+		id := strings.TrimPrefix(r.URL.Path, "/ws/")
+		HandleWebSocketConnection(w, r, ctx, id)
 	})
 
 	go http.ListenAndServe(":9098", nil)
 
 	fmt.Println("Start monitoring PostgreSQL...")
-	for {
-		WaitForNotification(listener)
-	}
+	/*	for {
+		WaitForNotification(listener, id)
+	}*/
 }
 
 func sendToClients(data []byte) {
@@ -99,22 +111,33 @@ func sendToClients(data []byte) {
 	}
 }
 
-func WaitForNotification(l *pq.Listener) {
+func WaitForNotification(l *pq.Listener, id string, conn *websocket.Conn) {
 	for {
 		select {
 		case n := <-l.Notify:
-			// Vous avez reçu une notification du canal PostgreSQL
-			fmt.Println("Received data from channel [", n.Channel, "] :", n.Extra)
+			// Analysez la notification pour obtenir sender_id
+			var msgData map[string]interface{}
+			if err := json.Unmarshal([]byte(n.Extra), &msgData); err != nil {
+				log.Println("Erreur lors de l'analyse de la notification:", err)
+				continue
+			}
 
-			// Vous pouvez envoyer ces données à tous les clients WebSocket
-			sendToClients([]byte(n.Extra))
+			fmt.Printf("ID : %s\n", id)
+
+			// Vérifiez si le sender_id correspond à l'id du WebSocket
+			if senderID, ok := msgData["sender_id"].(float64); ok && strconv.Itoa(int(senderID)) == id {
+				fmt.Println("Received data from channel [", n.Channel, "] :", n.Extra)
+				if err := conn.WriteMessage(websocket.TextMessage, []byte(n.Extra)); err != nil {
+					log.Println("Erreur lors de l'envoi du message WebSocket :", err)
+					conn.Close()
+					return
+				}
+			}
 
 		case <-time.After(10 * time.Second):
-
 			go func() {
 				l.Ping()
 			}()
-
 		}
 	}
 }
